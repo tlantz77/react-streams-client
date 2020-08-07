8f5bd8f85cf3af65d4accff52a6d8a960e4f5de5
Tim Lantz <tlantz@gmail.com>
2020-08-07 16:36:15 -0500

    install react router and set up placeholder links

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-function App() {
+import StreamCreate  from './streams/StreamCreate';
+import StreamEdit  from './streams/StreamEdit';
+import StreamShow  from './streams/StreamShow';
+import StreamList  from './streams/StreamList';
+import StreamDelete  from './streams/StreamDelete';
+
+
+const App = () => {
   return (
-    <div className="App">
-        App
+    <div>
+      <h1>Header</h1>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact component={StreamList}/>      
+          <Route path='/streams/new' exact component={StreamCreate}/>      
+          <Route path='/streams/edit' exact component={StreamEdit}/>      
+          <Route path='/streams/delete' exact component={StreamDelete}/>      
+          <Route path='/streams/show' exact component={StreamShow}/>      
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
