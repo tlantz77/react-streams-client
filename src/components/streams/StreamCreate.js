a8afb51f0f7d491b157d2d007444cab35f020344
Tim Lantz <tlantz@gmail.com>
2020-08-16 17:35:32 -0500

    add stream reducer

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
   renderError({ error, touched }) {
@@ -25,8 +27,8 @@ class StreamCreate extends Component {
     )
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -54,4 +56,6 @@ const validate = formValues => {
   return errors;
 };
  
-export default reduxForm({ form: 'streamCreate', validate })(StreamCreate);
+const formWrapped = reduxForm({ form: 'streamCreate', validate })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
