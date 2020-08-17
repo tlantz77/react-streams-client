243a019317f9391480234dac474d7bf4824bf824
Tim Lantz <tlantz@gmail.com>
2020-08-17 14:23:09 -0500

    refactor stream create with generic form component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-  return (
-    <div>StreamEdit</div>
-  );
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    const { stream } = this.props;
+    if (!stream) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>{stream.title}</div>
+    );
+  }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
  
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
