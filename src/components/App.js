056ac1f450eafffa30331168ee2e94cd75b5e071
Tim Lantz <tlantz@gmail.com>
2020-08-18 14:53:04 -0500

    implement stream show component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
@@ -13,13 +13,15 @@ const App = () => {
   return (
     <div className='ui container'>
       <Router history={history}>
-        <Header />
         <div>
-          <Route path='/' exact component={StreamList}/>      
-          <Route path='/streams/new' exact component={StreamCreate}/>      
-          <Route path='/streams/edit/:id' exact component={StreamEdit}/>      
-          <Route path='/streams/delete/:id' exact component={StreamDelete}/>      
-          <Route path='/streams/show' exact component={StreamShow}/>      
+          <Header />
+          <Switch>
+            <Route path='/' exact component={StreamList}/>      
+            <Route path='/streams/new' exact component={StreamCreate}/>      
+            <Route path='/streams/edit/:id' exact component={StreamEdit}/>      
+            <Route path='/streams/delete/:id' exact component={StreamDelete}/>      
+            <Route path='/streams/:id' exact component={StreamShow}/>      
+          </Switch>
         </div>
       </Router>
     </div>
